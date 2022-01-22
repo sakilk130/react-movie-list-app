a87ddf361d237fb49d651662e80277e75bbfc928
Sakil Khan <sakilk130@gmail.com>
2022-01-22 11:19:16 +0600

    hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
+
+//components
+import HeroImage from './HeroImage';
 
 // image
 import NoImage from '../images/no_image.jpg';
@@ -13,7 +16,17 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state, loading);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state?.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
