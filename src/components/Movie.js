2554375fe19f64895c1df7b77dad8067b403c484
Sakil Khan <sakilk130@gmail.com>
2022-01-24 13:33:33 +0600

    feat: actors name and image

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
+import Actor from './Actor';
 
 //image
 import NoImage from '../images/no_image.jpg';
@@ -32,6 +33,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
